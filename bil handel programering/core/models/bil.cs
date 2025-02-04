6bf385bd35834a2fd3b17da8ef52edf6bc70d857
Abdi Hassan <Abdimalik2002@outlook.dk>
2025-02-04 11:34:31 +0100

    i

---
+++ bil handel programering/core/models/bil.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bil_handel_programering.core.models
+{
+    internal class bil
+    {
+    }
+}
